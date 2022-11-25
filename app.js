2380e5537e4a2ea12cf94d6e510dc1235818eacf
00017192 <wiut17192@gmail.com>
2022-11-25 16:17:48 +0500

    feat: fully-comleted index.html

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
+// get header
 const header = document.querySelector("header");
 
 window.addEventListener("scroll", () => {
-  if (window.scrollY >= 130) {
-    header.classList.add("scrolled");
-    header.dataset.mode = "dark";
-  } else {
-    header.classList.remove("scrolled");
-    header.dataset.mode = "light";
+  if (!header.classList.contains("menu-opened")) {
+    if (window.scrollY >= 130) {
+      header.classList.add("scrolled");
+      header.dataset.mode = "dark";
+    } else {
+      header.classList.remove("scrolled");
+      header.dataset.mode = "light";
+    }
   }
+})
+
+header.querySelector(".menu-burger-container button").addEventListener("click", () => {
+  if (header.classList.contains("menu-opened")) {
+    header.dataset.mode = "light";
+  } else {
+    header.dataset.mode = "dark";
+  }
+  header.classList.toggle("menu-opened");
+  document.body.classList.toggle("stop-scrolling")
 })
