d46ae56a6f7c8af7a8e7ab73b8e705d985a83199
00017192 <wiut17192@gmail.com>
2022-11-26 00:01:51 +0500

    feat: complete project

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const header = document.querySelector("header");
 
 window.addEventListener("scroll", () => {
-  if (!header.classList.contains("menu-opened")) {
+  if (!header.classList.contains("menu-opened") && !header.classList.contains("other-page")) {
     if (window.scrollY >= 130) {
       header.classList.add("scrolled");
       header.dataset.mode = "dark";
@@ -14,11 +14,14 @@ window.addEventListener("scroll", () => {
 })
 
 header.querySelector(".menu-burger-container button").addEventListener("click", () => {
-  if (header.classList.contains("menu-opened")) {
-    header.dataset.mode = "light";
-  } else {
-    header.dataset.mode = "dark";
+  if (!header.classList.contains("other-page")) {
+    if (header.classList.contains("menu-opened")) {
+      header.dataset.mode = "light";
+    } else {
+      header.dataset.mode = "dark";
+    }
   }
+  
   header.classList.toggle("menu-opened");
   document.body.classList.toggle("stop-scrolling")
 })
